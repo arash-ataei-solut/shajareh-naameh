954906af2ce9c6d652b05f8394353d72ded73128
arash-ataei-solut <arash.ataee76@yahoo.com>
2025-03-29 21:43:05 +0330

    person list filter

--- users/filters.py
+++ users/filters.py
@@ -4,14 +4,11 @@ from persons.models import Person
 
 
 class PersonListFilter(django_filters.FilterSet):
-    first_name = django_filters.CharFilter(field_name='first_name', lookup_expr='icontains')
-    last_name = django_filters.CharFilter(field_name='last_name', lookup_expr='icontains')
-
     class Meta:
         model = Person
-        fields = [
-            'first_name',
-            'last_name',
-            'gender',
-            'birth_year',
-        ]
+        fields = {
+            'first_name': ['icontains'],
+            'last_name': ['icontains'],
+            'gender': ['exact'],
+            'birth_year': ['exact'],
+        }
