9aa1a795bb1f56b1ab18fa1c203b915a6827e20b
arash-ataei-solut <arash.ataee76@yahoo.com>
2025-04-10 17:18:55 +0330

    index

--- users/filters.py
+++ users/filters.py
@@ -1,14 +1,21 @@
 import django_filters
+from django.utils.translation import gettext as _
 
 from persons.models import Person
 
 
 class PersonListFilter(django_filters.FilterSet):
+    first_name = django_filters.CharFilter(field_name='first_name', lookup_expr='icontains', label=_('اسم'))
+    last_name = django_filters.CharFilter(field_name='last_name', lookup_expr='icontains', label=_('فامیلی'))
+    father_first_name = django_filters.CharFilter(
+        field_name='father__first_name', lookup_expr='icontains', label=_('اسم پدر')
+    )
+    mother_first_name = django_filters.CharFilter(
+        field_name='mother__first_name', lookup_expr='icontains', label=_('اسم مادر')
+    )
     class Meta:
         model = Person
         fields = {
-            'first_name': ['icontains'],
-            'last_name': ['icontains'],
             'gender': ['exact'],
             'birth_year': ['exact'],
         }
