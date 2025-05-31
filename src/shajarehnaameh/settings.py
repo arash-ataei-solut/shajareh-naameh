286071876711232190ebea8f214d1b6b1915381f
arash-ataei-solut <arash.ataee76@yahoo.com>
2025-05-31 22:09:55 +0330

    rename

--- src/shajarehnaameh/settings.py
+++ src/shajarehnaameh/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='treenameh.ir', cast=list)
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=['treenameh.ir'], cast=list)
 CSRF_COOKIE_SECURE = config('CSRF_COOKIE_SECURE', default=True, cast=bool)
 
 
